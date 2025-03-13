94fdd99f9266e8cdea5091c6c0feaad72124a051
mahdi <mahdighaleb196@>
2025-03-13 15:30:54 -0700

    Fix .gitignore

--- server/dist/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import authRoutes from '../auth-routes.js';
-import { ticketRouter } from '../api/ticket-routes.js';
-import { authenticateToken } from '../../middleware/auth.js';
-const router = Router();
-router.use('/auth', authRoutes);
-router.use('/tickets', authenticateToken, ticketRouter);
-export default router;
