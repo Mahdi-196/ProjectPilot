081aff82286e74f7e7dc4b92c86517fb1bf6742a
mahdi <mahdighaleb196@>
2025-03-14 16:14:16 -0700

    test env

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -4,8 +4,12 @@ import { sequelize } from '../models/index.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 const seedAll = async (): Promise<void> => {
   try {
+    console.log("Waiting 5 seconds for DB to be ready...");
+    await delay(5000);
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     
